7c82a7531acd6040222aa51fce68ce4db2136780
Kiwww17 <wimarizky17@gmail.com>
2026-07-15 15:39:08 +0700

    add erorr handling when fetch api

--- public/main.js
+++ public/main.js
@@ -4,6 +4,13 @@ const apiKey = "862e4353371419a4e19f23d14605568f";
 
 
 window.addEventListener('load', () => {
+    const hamburger = document.getElementById('hamburger');
+    const form = document.getElementById('form');
+    hamburger.addEventListener('click', () => {
+        form.classList.toggle('scale-0');
+        form.classList.toggle('scale-100');
+    });
+
     const city = document.getElementById('city');
     const cityName = localStorage.getItem('cityName' || '');
 
@@ -21,6 +28,7 @@ window.addEventListener('load', () => {
         const city = document.getElementById('city').value;
 
     })
+
 
     getFullDay((a, b, c) => {
         let day = '';
@@ -55,51 +63,71 @@ window.addEventListener('load', () => {
 
     async function runWeather() {
         const hasil = await getWeather(cityName);
-        const iconRaw = hasil.weather[0].main;
-        const descRaw = hasil.weather[0].description;
-        const wind = hasil.wind.speed;
-        const absTemp = hasil.main.temp;
-        const feelsTemp = hasil.main.feels_like;
-        const sunriseTimestamp = hasil.sys.sunrise;
+        
+        if (hasil.cod != '404') {
+            
+            const iconRaw = hasil.weather[0].main;
+            const descRaw = hasil.weather[0].description;
+            const wind = hasil.wind.speed;
+            const absTemp = hasil.main.temp;
+            const feelsTemp = hasil.main.feels_like;
+            const sunriseTimestamp = hasil.sys.sunrise;
 
-        const sunrise = new Date(sunriseTimestamp * 1000);
-        const formatter = new Intl.DateTimeFormat('id-ID', {
-            timeZone: 'Asia/Jakarta', 
-            hour: '2-digit',
-            minute: '2-digit',
-        });
-        
-        
-        let icon_ ='';
-        if(iconRaw == 'Thunderstorm'){
-            icon_ = './icon/cloud-bolt-solid-full.svg';
-        }else if(iconRaw == 'Drizzle'){
-            icon_ = './icon/cloud-rain-solid-full.svg';
-        }else if(iconRaw == 'Rain'){
-            icon_ = './icon/cloud-showers-heavy-solid-full.svg';
-        }else if(iconRaw == 'Snow'){
-            icon_ = './icon/snowflake-regular-full.svg';
-        }else if(iconRaw == 'Atmosphere'){
-            icon_ = './icon/smog-solid-full.svg';
-        }else if(iconRaw == 'Clear'){
-            icon_ = './icon/sun-solid-full.svg';
-        }else {
-            icon_ = './icon/cloud-solid-full.svg';
+            const sunrise = new Date(sunriseTimestamp * 1000);
+            const formatter = new Intl.DateTimeFormat('id-ID', {
+                timeZone: 'Asia/Jakarta',
+                hour: '2-digit',
+                minute: '2-digit',
+            });
+
+
+            let icon_ = '';
+            if (iconRaw == 'Thunderstorm') {
+                icon_ = './icon/cloud-bolt-solid-full.svg';
+            } else if (iconRaw == 'Drizzle') {
+                icon_ = './icon/cloud-rain-solid-full.svg';
+            } else if (iconRaw == 'Rain') {
+                icon_ = './icon/cloud-showers-heavy-solid-full.svg';
+            } else if (iconRaw == 'Snow') {
+                icon_ = './icon/snowflake-regular-full.svg';
+            } else if (iconRaw == 'Atmosphere') {
+                icon_ = './icon/smog-solid-full.svg';
+            } else if (iconRaw == 'Clear') {
+                icon_ = './icon/sun-solid-full.svg';
+            } else {
+                icon_ = './icon/cloud-solid-full.svg';
+            }
+
+            const icon = document.getElementById('icon');
+            const description = document.getElementById('description');
+            const displayTemp = document.getElementById('displayTemp');
+            const displaySunrise = document.getElementById('displaySunrise');
+            const displayWind = document.getElementById('displayWind');
+            const displayAbsTemp = document.getElementById('displayAbsTemp');
+
+            displayTemp.textContent = `${(feelsTemp - 273.15).toFixed(1)}°C`;
+            displaySunrise.textContent = formatter.format(sunrise);
+            displayWind.textContent = `${wind.toFixed(1)}m/s`;
+            displayAbsTemp.textContent = `${(absTemp - 273.15).toFixed(1)}°C`;
+            icon.innerHTML = `<img src=${icon_} alt='icon' class='w-[40%]'>`;
+            description.textContent = descRaw;
+        }else{
+            const icon = document.getElementById('icon');
+            const description = document.getElementById('description');
+            const displayTemp = document.getElementById('displayTemp');
+            const displaySunrise = document.getElementById('displaySunrise');
+            const displayWind = document.getElementById('displayWind');
+            const displayAbsTemp = document.getElementById('displayAbsTemp');
+
+            icon.textContent = ''
+            description.textContent = 'CITY NOT FOUND!'
+            description.classList.add('text-red-500', 'mt-8', 'font-bold')
+            description.classList.remove('text-gray-200')
+            displaySunrise.textContent = '-'
+            displayWind.textContent = '-'
+            displayAbsTemp.textContent = '-'
         }
 
-        const icon = document.getElementById('icon');
-        const description = document.getElementById('description');
-        const displayTemp = document.getElementById('displayTemp');
-        const displaySunrise = document.getElementById('displaySunrise');
-        const displayWind = document.getElementById('displayWind');
-        const displayAbsTemp = document.getElementById('displayAbsTemp');
-
-        displayTemp.textContent = `${(feelsTemp -273.15).toFixed(1)}°C`;
-        displaySunrise.textContent = formatter.format(sunrise);
-        displayWind.textContent = `${wind.toFixed(1)}m/s`;
-        displayAbsTemp.textContent = `${(absTemp - 273.15).toFixed(1)}°C`;
-        icon.innerHTML = `<img src=${icon_} alt='icon' class='w-[40%]'>`;
-        description.textContent = descRaw;
 
     }
     runWeather()
@@ -125,4 +153,5 @@ async function getWeather(city) {
     catch (erorr) {
         throw new Error("Couldn't fetch", error);
     }
+   
 }
